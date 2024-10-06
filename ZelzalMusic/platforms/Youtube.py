760b0feeeff6759007ee79cb207cb4a64e5c506b
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-10-06 15:53:43 +0300

    Update Youtube.py

--- ZelzalMusic/platforms/Youtube.py
+++ ZelzalMusic/platforms/Youtube.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 from ZelzalMusic.utils.database import is_on_off
-from ZelzalMusic.formatters import time_to_seconds
+from ZelzalMusic.utils.formatters import time_to_seconds
 
 
 
